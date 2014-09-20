0380804eed3cde300a3aa091bd2843c7811162af
Michael Feldstein <msfeldstein@gmail.com>
2014-09-19 19:47:46 -0700

    Make the copy javascripts recursive

--- AirFramerMac/copy-javascripts.js
+++ AirFramerMac/copy-javascripts.js
@@ -1,9 +1,24 @@
 fs = require('fs')
-var inDir = process.argv[2];
-var outDir = process.argv[3];
-var files = fs.readdirSync(inDir);
-for (var i = 0; i < files.length; ++i) {
-    if (files[i].indexOf('.js') == files[i].length - 3) {
-        fs.createReadStream(inDir + '/' + files[i]).pipe(fs.createWriteStream(outDir+'/'+files[i]))
+
+function copy(from, to) {
+    fs.createReadStream(from).pipe(fs.createWriteStream(to))
+}
+
+function copyDir(inDir, outDir) {
+    var files = fs.readdirSync(inDir);
+    for (var i = 0; i < files.length; ++i) {
+        if(fs.statSync(inDir + "/" + files[i]).isDirectory()) {
+            copyDir(inDir + "/" + files[i], outDir);
+        }
+    }
+    for (var i = 0; i < files.length; ++i) {
+        console.log(files[i])
+        if (files[i].indexOf('.js') == files[i].length - 3) {
+            copy(inDir + '/' + files[i], outDir+'/'+files[i]);
+        }
     }
 }
+
+var inDir = process.argv[2];
+var outDir = process.argv[3];
+copyDir(inDir, outDir)
